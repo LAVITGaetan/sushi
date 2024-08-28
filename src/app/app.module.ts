122d40d55979667122563260d11c793f463d942d
LAVITGaetan <g.lavit@outlook.fr>
2024-08-28 20:30:45 +0400

    add landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './elements/landing/landing.component';
+import { TestimonialsComponent } from './elements/testimonials/testimonials.component';
+import { GaleryComponent } from './elements/galery/galery.component';
+import { ProductsComponent } from './elements/products/products.component';
+import { FooterComponent } from './elements/footer/footer.component';
+import { NavbarComponent } from './elements/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    TestimonialsComponent,
+    GaleryComponent,
+    ProductsComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
